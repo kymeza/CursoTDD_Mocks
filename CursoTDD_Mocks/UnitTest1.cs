760a2195f9a81aa8a0ddab0d5936a68a1f00129b
Kyron Meza <kyron.meza@xerevro.com>
2021-09-22 16:57:16 -0300

    Agregar archivos de proyecto.

---
+++ CursoTDD_Mocks/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace CursoTDD_Mocks
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
